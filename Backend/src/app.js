c447d06bcd6396cd984711948e4072e3ad3185d2
riyarane46 <154919910+riyarane46@users.noreply.github.com>
2025-10-03 13:01:42 +0530

    zoom clone

---
+++ Backend/src/app.js
@@ -0,0 +1,28 @@
+import Express from 'express';
+import {createServer} from 'node:http';
+import  {Server} from 'socket.io';
+import cors from 'cors';
+import usersRoutes from './routes/users.routes.js';
+import mongoose from 'mongoose';
+import {connectToSocket} from './controllers/socketManager.js';
+
+const app = Express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(Express.json({limit: '40kb'}));
+app.use(Express.urlencoded({limit: '40kb', extended: true}));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://riyaVideo:Riyarane07@cluster0.kjdfh.mongodb.net/");
+    console.log(`Connected to database ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("Server is running on port 8000");
+    });
+}
+
+start();
